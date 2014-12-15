cf86e20456c9fd28e335d7a45a71649c90ba4a4c
ccyykobe <chenjohnson92@gmail.com>
2014-12-15 11:08:14 +0800

    把上面的actionbar基本完成，请测试
    无注释无测试版。。。

---
+++ ShenmaApp/app/src/main/java/com/example/john/shenmaapp/KidFragment.java
@@ -0,0 +1,9 @@
+package com.example.john.shenmaapp;
+
+import android.app.Fragment;
+
+/**
+ * Created by John on 12/15/2014.
+ */
+public class KidFragment extends Fragment{
+}
---
+++ ShenmaApp/app/src/main/java/com/example/john/shenmaapp/StuFragment.java
@@ -0,0 +1,10 @@
+package com.example.john.shenmaapp;
+
+import android.app.Fragment;
+
+/**
+ * Created by John on 12/15/2014.
+ */
+public class StuFragment extends Fragment {
+
+}
