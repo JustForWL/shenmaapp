26996a7c1fc35d080a337a0a939fe17d63644156
YongHui <yonghui_chad@163.com>
2014-12-15 19:35:36 +0800

    eclipse 编译通过

--- ShenmaApp/app/src/main/java/com/example/john/shenmaapp/KidFragment.java
+++
@@ -1,9 +0,0 @@
-package com.example.john.shenmaapp;
-
-import android.app.Fragment;
-
-/**
- * Created by John on 12/15/2014.
- */
-public class KidFragment extends Fragment{
-}
---
+++ shenmaApp/src/com/example/shenmaapp/TchFragment.java
@@ -0,0 +1,9 @@
+package com.example.shenmaapp;
+
+import android.app.Fragment;
+
+/**
+ * Created by John on 12/15/2014.
+ */
+public class TchFragment extends Fragment{
+}
